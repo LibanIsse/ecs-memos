6edd90e095692f4c4389c3601ed4be4436a9910c
LibanIsse <liban_isse@icloud.com>
2026-05-08 17:58:06 +0100

    build ECS image for amd64

--- infra/modules/ecs/main.tf
+++ infra/modules/ecs/main.tf
@@ -27,7 +27,7 @@ resource "aws_ecs_task_definition" "memos" {
   container_definitions = jsonencode([
     {
       name      = "memos"
-      image     = "527814729206.dkr.ecr.eu-west-2.amazonaws.com/ecsmemos:d9d95fef5eed1b4db0bdf7ebb08a1b47a0daf27f"
+      image     = "527814729206.dkr.ecr.eu-west-2.amazonaws.com/ecsmemos:amd64-fix"
       essential = true
 
       portMappings = [
