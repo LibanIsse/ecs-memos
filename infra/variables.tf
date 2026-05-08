a51d4356e7af727afd69c9cea10c45913b3cce2c
LibanIsse <liban_isse@icloud.com>
2026-05-08 15:56:32 +0100

    pass DB password to github secrets

--- infra/variables.tf
+++ infra/variables.tf
@@ -86,3 +86,9 @@ variable "hosted_zone_name" {
   description = "domain name"
   type        = string
 }
+
+variable "db_password" {
+  description = "Database RDS password"
+  type        = string
+  sensitive   = true
+}
