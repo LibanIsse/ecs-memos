bd0f5013c456fc5dadbe694bfd5382d06f2daa4f
LibanIsse <liban_isse@icloud.com>
2026-05-09 17:22:05 +0100

    add drift detection workflow

--- infra/modules/vpc/main.tf
+++ infra/modules/vpc/main.tf
@@ -167,6 +167,7 @@ resource "aws_vpc_security_group_egress_rule" "allow_alb_out" {
   security_group_id = aws_security_group.alb_sg.id
   cidr_ipv4         = "0.0.0.0/0"
   ip_protocol       = "-1" # semantically equivalent to all ports
+  description       = "Allow outbound traffic from the ALB"
 }
 
 
