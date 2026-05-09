1e1360ed3433d9158ef3e8b02f80b29cab9b6fb1
LibanIsse <liban_isse@icloud.com>
2026-05-09 17:56:04 +0100

    add variables for ECR

--- infra/modules/ecr/main.tf
+++ infra/modules/ecr/main.tf
@@ -1,7 +1,7 @@
 #create ecr repository
 resource "aws_ecr_repository" "memos_repo" {
-  name                 = "ecsmemos"
-  image_tag_mutability = "MUTABLE"
+  name                 = var.repository_name
+  image_tag_mutability = var.image_tag_mutability
   force_delete         = true
 
   image_scanning_configuration {
